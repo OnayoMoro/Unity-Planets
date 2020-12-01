be846986a891fca1a795a5e9ec7aaa0a23755709
OnayoMoro <45406920+OnayoMoro@users.noreply.github.com>
2020-12-01 22:20:57 +0000

    Finished basic flight mechanics/camera momentum
    Feels better I think, idk

--- Planet Generation/Assets/PlayerFuntion/Interaction.cs
+++ Planet Generation/Assets/PlayerFuntion/Interaction.cs
@@ -11,8 +11,6 @@ public class Interaction : MonoBehaviour
     bool pressed = true;
     Vector3 player_pos = new Vector3(0f, 0.57f, -0.03f);
     Vector3 player_rotation = new Vector3(0,0,0);
-    Vector3 ship_pos = new Vector3(0f, -1.008f, 2.21f);
-    Vector3 ship_rotation = new Vector3(-90, 180, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -36,37 +34,22 @@ public class Interaction : MonoBehaviour
             //Disable ship gravity interaction and add gimble control (drag)
             Ship.GetComponent<Attractor>().enabled = false;
             Ship.GetComponent<Rigidbody>().drag = 2;
-            Ship.GetComponent<Rigidbody>().angularDrag = 5;
+            Ship.GetComponent<Rigidbody>().angularDrag = .4f;
 
             //Change camera to ship view
             Camera.transform.parent = null;
-
-            //Vector3 moveCamTo_pos = Ship.transform.localPosition - Ship.transform.forward * 10f + Ship.transform.up * 5f;
-            //Vector3 moveCamTo_rotation = Ship.transform.eulerAngles + ship_rotation;
-
-            //Camera.transform.position = moveCamTo_pos;
-            //Camera.main.transform.LookAt(Ship.transform.position);
-            //Camera.transform.LookAt(Ship.transform.eulerAngles);
-            //Camera.transform.parent = Ship.transform;
-            //Camera.transform.localPosition = ship_pos;
-            //Camera.transform.localEulerAngles = ship_rotation;
-
         }
 
         else if (pressed == false)
         {
-            Vector3 moveCamTo_pos = Ship.transform.localPosition - Ship.transform.up * 20f + Ship.transform.forward * 5f + Ship.transform.right;
-            Vector3 moveCamTo_rotation = Ship.transform.eulerAngles + ship_rotation;
+            Vector3 moveCamTo_pos = Ship.transform.localPosition - Ship.transform.forward * 20f + Ship.transform.up * 5f;
 
-            float bias = .92f;
+            float bias = .90f;
 
             Camera.transform.position = Camera.transform.position * bias + moveCamTo_pos * (1f - bias);
-            Camera.transform.LookAt(Ship.transform.localPosition + Ship.transform.forward * 10f + Ship.transform.up + Ship.transform.right);
-            //Camera.transform.Rota
-            //Camera.transform.Translate(Ship.transform.localRotation);
+            Camera.transform.LookAt(Ship.transform.localPosition + Ship.transform.up * 10f + Ship.transform.forward + Ship.transform.right);
             Quaternion ting = new Quaternion(Ship.transform.rotation.x, Ship.transform.rotation.y, Ship.transform.rotation.z, Ship.transform.rotation.w);
             Camera.transform.rotation = ting;
-            //Camera.transform.rotation.x = Ship.transform.rotation.x;
 
             if (Input.GetKeyDown(KeyCode.F) && pressed == false)
             {
--- Planet Generation/Assets/Ship Control/ShipTestControl.cs
+++ Planet Generation/Assets/Ship Control/ShipTestControl.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class ShipTestControl : MonoBehaviour
 {
     public GameObject Ship;
+    //public GameObject Camera;
 
     //Camera rotation vars
-    public float mouseSensitivityX = 40;
-    public float mouseSensitivityY = 20;
-    float verticalLookRotation;
-    float horizontalLookRotation;
+    public float mouseSensitivityX = 1f;
+    public float mouseSensitivityY = 1f;
     Transform cameraTransform;
+
+    float mousePosX = 0;
+    float mousePosY = 0;
 
     void Awake()
     {
@@ -20,64 +22,80 @@ public class ShipTestControl : MonoBehaviour
     }
 
     // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
+
+        mousePosX = 0;
+        mousePosY = 0;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // Look rotation:
-        transform.Rotate(Vector3.forward * Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime *.017f);
-        transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime *.017f);
+
+        mousePosX = Input.GetAxis("Mouse X") + mousePosX /** mouseSensitivityX*/;
+        mousePosY = -Input.GetAxis("Mouse Y") + mousePosY /** mouseSensitivityY*/;
+
+        if (mousePosX > 160) { mousePosX = 160; }
+        else if (mousePosX < -160) { mousePosX = -160; }
+
+        if (mousePosY > 160) { mousePosY = 160; }
+        else if (mousePosY < -160) { mousePosY = -160; }
+
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(mousePosY / 100, mousePosX / 100, 0f);
+        //transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime *.017f);
+        //transform.Rotate(Vector3.right * -Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime *.017f);
 
         //Move ship up
         if (Input.GetKey(KeyCode.Space))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.forward * 50000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.up * 10000);
         }
 
         //Move ship down
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.forward * -50000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.up * -10000);
         }
 
         //Move ship forward 
         if (Input.GetKey(KeyCode.W))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.up * 300000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.forward * 50000);
         }
 
         //Move ship backwards
         if (Input.GetKey(KeyCode.S))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.up * -50000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.forward * -50000);
         }
 
         //Strafe ship left
         if (Input.GetKey(KeyCode.A))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.right * 50000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.right * -10000);
         }
 
         //Strafe ship right
         if (Input.GetKey(KeyCode.D))
         {
-            Ship.GetComponent<Rigidbody>().AddForce(transform.right * -50000);
+            Ship.GetComponent<Rigidbody>().AddForce(transform.right * 10000);
         }
 
         //Rotate ship right
         if (Input.GetKey(KeyCode.E))
         {
-            Ship.GetComponent<Rigidbody>().AddTorque(transform.up * -30000);
+            Ship.GetComponent<Rigidbody>().AddTorque(transform.forward * -3000);
         }
 
         //Rotate ship right
         if (Input.GetKey(KeyCode.Q))
         {
-            Ship.GetComponent<Rigidbody>().AddTorque(transform.up * 30000f);
+            Ship.GetComponent<Rigidbody>().AddTorque(transform.forward * 3000f);
         }
         //otherObject.GetComponent(NameOfScript).enabled = false;
     }
